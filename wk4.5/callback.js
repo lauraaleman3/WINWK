ef6b265d76a8687e861818ba93748d15c47e009a
Laura Aleman <79146743+lauraaleman3@users.noreply.github.com>
2021-03-14 16:51:37 -0500

    commit to callback project
    added sort, shuffle, css background

--- wk4.5/callback.js
+++ wk4.5/callback.js
@@ -5,11 +5,38 @@ let ourNames = document.querySelector('#ourNames');
 //Array of names
 let names = ['Laura', 'Eyke', 'April', 'Echu', 'Jonathan'];
 
-//Sorts the names array
+//Function to sort the names array
 function sortNames() {
   let sorted =  names.sort().join(', ');
-  ourNames.innerHTML = sorted;
+  // // added <br /> twice for 2 breaks between names and greeting
+  ourNames.innerHTML = `Sorted: <br /> <br /> ${sorted}`;
 }
 
-//sortNames as a Callback
-btn.addEventListener('click', sortNames);
+
+  // // Button to sort with Greeting
+  btn.addEventListener('click', sortNames);
+
+// //.........SHUFFLE NAMES STARTS.............
+
+// // LINK SHUFFLE BUTTON
+let shuffleBtn = document.querySelector('#shuffleBtn');
+
+// //SHUFFLE FUNCTION
+function shuffleArray() {
+  let shufNames = [' Laura', ' Eyke', ' April', ' Echu', ' Jonathan'];
+  for (let i = shufNames.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shufNames[i], shufNames[j]] = [shufNames[j], shufNames[i]];
+  }
+  document.getElementById("ourNames").innerHTML = `Shuffled: <br /> <br /> ${shufNames}`;
+}
+
+
+
+  // // Button to shuffle with Greeting
+  shuffleBtn.addEventListener('click', shuffleArray);
+function greetshuff() {
+  document.getElementById("ourNames").innerHTML = `Shuffled: <br /> <br /> ${names}`;
+}
+
+
