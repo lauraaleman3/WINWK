bc4ddcc21348b3cffa3a6703a06d2f5096e8928a
Laura Aleman <79146743+lauraaleman3@users.noreply.github.com>
2021-03-15 15:04:39 -0500

    commit update cities
    updated cities in code

--- wk5.1/hw/index.js
+++ wk5.1/hw/index.js
@@ -1,5 +1,4 @@
 // //variable declarations
-
     longt = document.getElementById("longt");
     lat = document.getElementById("latt");
 
@@ -26,12 +25,20 @@ async function city(cityName) {
   })
   .catch((error) => console.log("error", error));
 }
-
 /*// Outputs Houston longitude and lattitude in console and on page as:
  -95.39836
  29.77447   */
-city('houston');
+// city('houston');
 
+/*// coordinates for Charlott:
+ -80.80483
+index.js:23 35.19317 */
+// city('charlotte');
+
+/*// coordinates for seattle:
+-122.33279
+index.js:23 47.62405 */
+city('seattle');
 
 
 
